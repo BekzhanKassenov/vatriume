295b193ce7389679c0036de9773d067d61b75268
Kirill Kirillov <kirusfg@gmail.com>
2023-11-24 21:14:45 +0600

    fix: add emoji support

--- public/api/db.php
+++ public/api/db.php
@@ -7,6 +7,9 @@ function connect_to_db_or_die() {
     // All database credentials are defined in secret.php
     $db_conn = new mysqli($db_server, $db_username, $db_password, $db_name, $db_port);
 
+    // Support 4-bytes-long UTF8 characters, including emojis
+    $db_conn->set_charset('utf8mb4');
+
     // Check connection
     if (mysqli_connect_error()) {
         header('HTTP/1.1 503 Unavailable');
--- sql/create_db.sql
+++ sql/create_db.sql
@@ -1,6 +1,6 @@
  CREATE TABLE suggestions (
      id SERIAL, 
-     text LONGTEXT CHARACTER SET utf8, 
+     text LONGTEXT CHARACTER SET utf8mb4,
      destination VARCHAR(20), 
      timestamp VARCHAR(100)
  );
